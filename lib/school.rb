ce8dc383cc69b481f0b4bc723aea13a444863798
mario acevedo <jun_acevedo415@yahoo.com>
2020-07-12 05:07:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,21 +1,9 @@
-class School
-  attr_accessor :name, :roster
-  
-  def initialize(name)
-    @name = name
-    @roster = {}
+def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
   end 
+  nu_hash
+end 
   
-  def add_student(student, level)
-    
-    roster[level] ||= []
-    roster[level] << student
-  end 
-  
-  def grade(level)
-    roster.detect do |x, y| 
-      if x == level
-        return y 
-      end 
-    end 
-  end 
+endlearn
