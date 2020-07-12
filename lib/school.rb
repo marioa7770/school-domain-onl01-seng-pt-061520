eb7dc0770474fdf170361894b4e475b98cc4a484
mario acevedo <jun_acevedo415@yahoo.com>
2020-07-12 05:22:33 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,27 +1,25 @@
 class School
-  def initialize(name)
+  attr_accessor :name, :roster
+
+  def initialize(name) 
     @name = name
     @roster = {}
   end
 
-  def roster
-    @roster
+  def add_student(student_name, grade) #part 2a
+    roster[grade] ||= [] 
+    roster[grade] << student_name 
   end
 
-  def add_student(name, grade)
-    if @roster[grade] != nil
-      @roster[grade] << name
-    else
-      @roster[grade] = [name]
+  def grade(student_grade) 
+    roster[student_grade]
+  end
+
+  def sort  
+    sorted = {} 
+    roster.each do |grade, students| 
+      sorted[grade] = students.sort 
     end
+    sorted
   end
-
-  def grade(grade)
-    @roster[grade]
-  end
-
-  def sort
-    @roster.each do |key, value|
-      value.sort!
-    end
-  end
+end
